b1f53a1fd839c79dae1245bfef9ada65e94d6eb9
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2024-03-31 20:12:33 +0200

    Actualizar kernel.c

--- kernel.c
+++ kernel.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Definición de las funciones del microkernel
+void iniciarSistemaOperativo();
+void cargarAdministradorMemoria();
+void cargarAdministradorProcesos();
+void cargarSistemaArchivos();
+void iniciarServiciosEsenciales();
+void ejecutarSistemaOperativo();
+void detenerSistemaOperativo();
+
+int main() {
+    iniciarSistemaOperativo();
+    cargarAdministradorMemoria();
+    cargarAdministradorProcesos();
+    cargarSistemaArchivos();
+    iniciarServiciosEsenciales();
+    ejecutarSistemaOperativo();
+    detenerSistemaOperativo();
+
+    return 0;
+}
+
+void iniciarSistemaOperativo() {
+    printf("Iniciando microkernel...\n");
+}
+
+void cargarAdministradorMemoria() {
+    printf("Cargando administrador de memoria...\n");
+}
+
+void cargarAdministradorProcesos() {
+    printf("Cargando administrador de procesos...\n");
+}
+
+void cargarSistemaArchivos() {
+    printf("Cargando sistema de archivos...\n");
+}
+
+void iniciarServiciosEsenciales() {
+    printf("Iniciando servicios esenciales...\n");
+}
+
+void ejecutarSistemaOperativo() {
+    printf("Ejecutando sistema operativo...\n");
+    // Aquí se ejecutaría el bucle principal del sistema operativo
+}
+
+void detenerSistemaOperativo() {
+    printf("Deteniendo sistema operativo...\n");
+}
